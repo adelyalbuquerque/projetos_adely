d998a4ccda7203d76b45f87af020041433155cac
Adely Albuquerque <adely.albuquerque@gmail.com>
2016-07-18 10:42:11 -0300

    Reversor de numeros com teste

--- exercicios_funcoes/reversor_numero/reversor_de_numeros.py
+++ exercicios_funcoes/reversor_numero/reversor_de_numeros.py
@@ -1,4 +1,5 @@
 def reversor_de_numeros(numero):
     numero = str(numero)
     numero_reverso = numero[-1::-1]
+    numero_reverso = int(numero_reverso)
     return numero_reverso
--- exercicios_funcoes/reversor_numero/test_reversor_de_numeros.py
+++ exercicios_funcoes/reversor_numero/test_reversor_de_numeros.py
@@ -1,3 +1,3 @@
 from reversor_de_numeros import reversor_de_numeros
 def test_reversor_de_numeros():
-    assert reversor_de_numeros(12) == "21"
+    assert reversor_de_numeros(12) == 21

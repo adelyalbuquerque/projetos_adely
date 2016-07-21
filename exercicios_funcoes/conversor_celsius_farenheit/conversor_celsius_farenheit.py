0adb769cbc05a79f6f155bd3faae76c417828d88
Adely Albuquerque <adely.albuquerque@gmail.com>
2016-07-21 18:08:01 -0300

    Conversor celsius para farenheit com teste

---
+++ exercicios_funcoes/conversor_celsius_farenheit/conversor_celsius_farenheit.py
@@ -0,0 +1,4 @@
+def conversor_celsius_farenheit(graus_celsius):
+    graus_celsius = float(graus_celsius)
+    graus_f= (graus_celsius * 1.8) + 32
+    return graus_f
---
+++ exercicios_funcoes/conversor_celsius_farenheit/test_conversor_celsius_farenheit.py
@@ -0,0 +1,3 @@
+from conversor_celsius_farenheit import conversor_celsius_farenheit
+def test_conversor_celsius_farenheit():
+    assert conversor_celsius_farenheit(35) == 95

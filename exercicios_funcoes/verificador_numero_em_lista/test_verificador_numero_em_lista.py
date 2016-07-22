e0c17b58fe3f987be96ea69037795128a5d3d502
Adely Albuquerque <adely.albuquerque@gmail.com>
2016-07-21 21:30:14 -0300

    Verificador de numero com teste False

--- exercicios_funcoes/verificador_numero_em_lista/test_verificador_numero_em_lista.py
+++ exercicios_funcoes/verificador_numero_em_lista/test_verificador_numero_em_lista.py
@@ -1,3 +1,3 @@
 from verificador_numero_em_lista import verificador_numero_em_lista
 def test_verificador_numero_em_lista():
-    assert verificador_numero_em_lista((1,2,3), 3) == True
+    assert verificador_numero_em_lista((1,2,3), 5) == False
